e9ee8325b4b5cb2515d602c6d46781bc9070f499
Saqifur-Rahman <85213128+Saqifur-Rahman@users.noreply.github.com>
2021-06-15 02:41:37 +0530

    add-columns completed

--- script.js
+++ script.js
@@ -4,11 +4,50 @@ let colorSelected;
 
 //Adds a row
 function addR() {
+
     alert("Clicked Add Row")
+    
+    // check if columns = 0
+    if(numCols == 0)
+        numCols++
+
+    // insert row
+    var row = grid.insertRow() 
+    grid.appendChild(row);
+
+    // insert cells
+    for (var i = 0; i < numCols; i++) {
+        // Add Cell 
+        var newCell = row.insertCell(i); 
+        var newText = document.createTextNode('CELL'); 
+        newCell.appendChild(newText);
+    }
+    numRows++;
+
 }
+
 //Adds a column
 function addC() {
+
     alert("Clicked Add Col")
+
+    // check if rows = 0
+    if(numRows == 0) {
+        return addR()
+    }
+    
+    // insert cells
+    // outer loop - columns
+    for (var i = numCols++; i < numCols; i++) {
+        // inner loop - rows
+        for (var j=0; j < numRows; j++) {
+            // Add Cell 
+            var newCell = grid.rows[j].insertCell(i); 
+            var newText = document.createTextNode('CELL'); 
+            newCell.appendChild(newText);
+        }
+    }
+
 }
 
 //Removes a row
